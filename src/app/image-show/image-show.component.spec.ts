90aac8d6421ee623dba95eb70bb33dc6f12d962e
Arijit0 <bagarijit0@gmail.com>
2020-11-19 14:57:47 +0530

    Updating progress

--- src/app/image-show/image-show.component.spec.ts
+++ src/app/image-show/image-show.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImageShowComponent } from './image-show.component';
 
@@ -6,7 +6,7 @@ describe('ImageShowComponent', () => {
   let component: ImageShowComponent;
   let fixture: ComponentFixture<ImageShowComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ImageShowComponent ]
     })
